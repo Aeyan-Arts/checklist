9a0352109d1821af14bbfd02f4366f966255ecc7
philoo123 <philofayek987@gmail.com>
2024-06-29 14:16:21 +0300

    Update script.js

---
+++ checklist/script.js
@@ -0,0 +1,9 @@
+
+const menu = document.createElement('li');
+document.appendChild(menu);
+
+const lol = document.getElementsByTagName('label');
+lol.innerhtml('To Do list');
+
+let bu = document.getElementById('bu1');
+
