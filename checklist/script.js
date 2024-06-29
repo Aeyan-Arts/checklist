08fee2b45382955cea720f9f94e9aa8090185b82
philoo123 <philofayek987@gmail.com>
2024-06-29 14:31:06 +0300

    Update script.js

--- checklist/script.js
+++ checklist/script.js
@@ -1,9 +1,24 @@
 
-const menu = document.createElement('li');
-document.appendChild(menu);
+// const menu = document.createElement('li');
+// document.appendChild(menu);
 
-const lol = document.getElementsByTagName('label');
-lol.innerhtml('To Do list');
+// const lol = document.getElementsByTagName('label');
+// lol.innerhtml('To Do list');
 
-let bu = document.getElementById('bu1');
+function addItem(){
+	var ul = document.getElementById("myList");
+  var newItem = document.getElementById("newItem");
+  var li = document.createElement("li");
+  li.setAttribute('id',newItem.value);
+  li.appendChild(document.createTextNode(newItem.value));
+  ul.appendChild(li);
+}
+
+function removeItem(){
+	var ul = document.getElementById("myList");
+  var newItem = document.getElementById("newItem");
+  var item = document.getElementById(newItem.value);
+  ul.removeChild(item);
+}
+
 
